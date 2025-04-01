acd8e7d326552f39d48a047723289f22f2aed569
Stas <lYagami.Light@gmail.com>
2025-04-02 01:37:00 +0500

    Commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,8 +1,15 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+
+import static java.sql.JDBCType.BIGINT;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
@@ -10,14 +17,40 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
+        try(Connection connection = new Util().getConnection();
+            Statement statement = connection.createStatement()) {
+            String sql = "Create table if not exists users (id BIGINT AUTO_INCRIMENT PRIMARY KEY, name VARCHAR(255), lastname VARCHAR(255), age TYNYINT)";
+            statement.executeUpdate(sql);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
     public void dropUsersTable() {
+        try(Connection connection = new Util().getConnection();
+            Statement statement = connection.createStatement()) {
+            String sql = "Drop table if exists users";
+            statement.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String sql = "insert into users (name, lastname, age) values (?, ?, ?)";
+            try(Connection connection = new Util().getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setByte(3, age);
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,12 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+    }
 }
